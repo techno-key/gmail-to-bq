16e982b65dc5f0b3ea4a1fe01ff3fb3a92695daf
Harsh Chaudhary <chaudharyharsh311@gmail.com>
2022-10-07 15:10:14 +0530

    first push

---
+++ app.py
@@ -0,0 +1,186 @@
+from fileinput import filename
+import imaplib
+import pandas as pd
+from datetime import datetime, timedelta
+import pytz
+import email
+import io
+from google.oauth2 import service_account
+from google.cloud import bigquery
+from utils import unzip_csv , send_email, get_uploadparams
+import pandas_gbq
+import csv
+from urllib.request import urlopen
+
+
+uploadparms_sheet, upload_params = get_uploadparams()
+
+# gmail account credentials
+#username=upload_params.loc[param_ID,'Username']
+#password=upload_params.loc[param_ID,'Password']
+
+
+class Email_To_BQ():
+    def __init__(self, start_date, param_ID):
+        self.start_date = start_date
+        self.param_ID = param_ID
+        self.mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        self.mail.login(upload_params.loc[param_ID,'Username'], upload_params.loc[param_ID,'Password'])
+        self.mail.select("inbox") # connect to inbox.
+        self.search_query = '(FROM "'+upload_params.loc[param_ID,'From_Email']+'" SUBJECT "'+upload_params.loc[param_ID,'Search Query']+'") SINCE "' + start_date + '"'
+        self.already_ingested_files = list(pd.read_csv('Ingested_files/' + upload_params.loc[param_ID, 'Ingested File Name'])['filename'])        # column name in ingested file
+
+    def our_id_list(self):
+        result, data = self.mail.search(None, self.search_query)
+        ids = data[0] # data is a list.
+        id_list = ids.split() # ids is a space separated string
+        print("id_list = ", id_list)
+        return id_list
+
+    def run(self):
+        id_list = self.our_id_list()
+
+        # for email_id in reversed(id_list):
+        for email_id in id_list:
+            result, data = self.mail.fetch(email_id, "(RFC822)")
+            raw_email = data[0][1]
+            
+            datestring = email.message_from_bytes(raw_email)['date']        # extracting date and time of email
+            datetime_obj = email.utils.parsedate_to_datetime(datestring)
+            email_date = datetime_obj#.date()
+            print('Email date: ',email_date)
+
+        # converts byte literal to string removing b''
+            raw_email_string = raw_email.decode('utf-8')
+            email_message = email.message_from_string(raw_email_string)
+            for header in [ 'subject' ]:
+                subject_ = email_message[header]
+                # print( (header.upper(), email_message[header]))
+                #email_date = subject_.split(".csv")[0][-10:]
+                #print(email_date)
+
+            for part in email_message.walk():  # goes inside the mail to check for attachment
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+
+                if upload_params.loc[self.param_ID, 'File Type'] in ['url_zip','url_csv','url_excel']:      # if data is in link, search for link in email body
+                    if part.get_content_type() == 'text/plain':
+                        plain_text = part.get_payload()
+                        word_list = plain_text.split()
+                        file_url = [k for k in word_list if ('https:' in k) or ('http:' in k)][0].lstrip('<').rstrip('>')
+                        print('file url:',file_url)
+                        fileName = file_url.split('/')[-1]
+                else:
+                    fileName = part.get_filename()
+                print("fileName = ", fileName)
+
+                if (fileName not in self.already_ingested_files) & (str(upload_params.loc[param_ID, 'Attachment File Name']).lower() in str(fileName).lower()):
+                    print(fileName)
+                    #print(fileName.split(".csv")[0][-10:])
+                    try:
+                        if upload_params.loc[self.param_ID, 'File Type'] in ['url_zip','url_csv','url_excel']:
+                            resp = urlopen(file_url)
+                            file_bytes = io.BytesIO(resp.read())
+                        else:
+                            file_bytes = io.BytesIO(part.get_payload(decode=True))
+
+                        if upload_params.loc[self.param_ID, 'File Type'] in ['zip','url_zip']:
+                            csv_file = unzip_csv(file_bytes)  # unzipping the file returning final csv data
+                            df = pd.read_csv(csv_file)
+                        elif upload_params.loc[self.param_ID, 'File Type'] in ['csv','url_csv']:
+                            csv_file = file_bytes
+                            df = pd.read_csv(csv_file)
+                        elif upload_params.loc[self.param_ID, 'File Type'] in ['excel','url_excel']:
+                            excel_file = file_bytes
+                            df = pd.read_excel(excel_file)
+                        else:
+                            print("Invalid file type")
+
+                        df['updated_datetime'] = email_date
+                        df = df.astype(str)
+                        df = df.fillna('')
+                        print(df.head())
+                        credentials = service_account.Credentials.from_service_account_file('Cred_files/' + upload_params.loc[self.param_ID,'Credential Json File'],)
+                        bq_client = bigquery.Client(credentials=credentials, project=upload_params.loc[self.param_ID,'Project ID'])
+                        print(df.head())
+                        print('columns = ', df.columns)
+                        df = df.loc[:,upload_params.loc[self.param_ID,'Input Column Names'].split(',') + ['updated_datetime']]
+                        df.columns = upload_params.loc[self.param_ID,'Destination Column Names'].split(',') + ['updated_datetime']
+                        
+                        df['updated_datetime'] = df['updated_datetime'].apply(lambda x: pd.Timestamp(x, tz='Asia/Kolkata'))    #converting string back to timestamp
+                        
+                        # Uploading data to gbq
+                        pandas_gbq.to_gbq(df, upload_params.loc[self.param_ID,'Destination Table'], upload_params.loc[self.param_ID,'Project ID'], 
+                                                credentials=credentials, if_exists=upload_params.loc[self.param_ID, 'IF Exists'])
+
+                        print("  ", df.head())
+
+
+                        ing_file = upload_params.loc[self.param_ID, 'Ingested File Name']
+                        with open('Ingested_files/'+ing_file, 'a') as f:
+                            writer = csv.writer(f)
+                            writer.writerow([fileName])
+
+                        upload_params.loc[self.param_ID, 'Last Run Date'] = today_date.strftime('%Y-%m-%d')      #updating last run date
+                        upload_params.loc[self.param_ID, 'Last Run Hour'] = hour_now                             #updating last run hour
+
+
+                    except Exception as e:
+                        dest_table = upload_params.loc[self.param_ID,'Destination Table']
+                        send_email(from_email=upload_params.loc[self.param_ID,'Error_From_Email'],                   #send an email if there is an error in uploading
+                                    from_email_pass=upload_params.loc[self.param_ID,'Error_From_Email_Password'],
+                                    to_email=upload_params.loc[self.param_ID,'Error_To_Email'],
+                                    subject='Error in Uploading file(s)', 
+                                    body_text=f'There was an error in uploading the file to {dest_table} : {e}')
+                        print('--Error--\n')
+                        print(e)
+
+
+
+
+
+if __name__ == '__main__':
+
+    param_id_list = upload_params.index.to_list()
+    for param_ID in param_id_list:
+        #date = (datetime.today().date() - timedelta(days = 2)).strftime('%d-%b-%Y')
+        #print(date)
+        last_run_date = (upload_params.loc[param_ID, 'Last Run Date'].date() - timedelta(days = 0)).strftime('%d-%b-%Y')    #last run date
+        today_date = datetime.today().date()                                #today's date
+        hour_now = datetime.now(pytz.timezone('Asia/Kolkata')).time().hour      #time now (hour)
+        print("Today's date:",today_date, "\nLast Run date:",upload_params.loc[param_ID, 'Last Run Date'].date())
+
+        # For Daily Uploading or uploading with fixed day difference 
+        if upload_params.loc[param_ID, 'Run Type'] == 'Daily':
+            run_gap = int(upload_params.loc[param_ID, 'Run Gap'])
+
+            if (upload_params.loc[param_ID, 'Last Run Date'].date() + timedelta(days = run_gap) <= today_date) & (upload_params.loc[param_ID, 'Time to Run'] <= hour_now):
+                print('Running...')
+                obj = Email_To_BQ(last_run_date,param_ID=param_ID)
+                obj.run()
+                #upload_params.loc[param_ID, 'Last Run Date'] = today_date.strftime('%Y-%m-%d')      #updating last run date
+            else:
+                pass
+
+        # For Hourly Uploading or uploading with fixed hour difference
+        elif upload_params.loc[param_ID, 'Run Type'] == 'Hourly':
+            run_gap = int(upload_params.loc[param_ID, 'Run Gap'])
+
+            if upload_params.loc[param_ID, 'Last Run Date'].date() < today_date:
+                print('Running...')
+                obj = Email_To_BQ(last_run_date,param_ID=param_ID)
+                obj.run()
+            elif (upload_params.loc[param_ID, 'Last Run Date'].date() == today_date) & (int(upload_params.loc[param_ID, 'Last Run Hour']) + int(run_gap) <= hour_now):
+                print('Running...')
+                obj = Email_To_BQ(last_run_date,param_ID=param_ID)
+                obj.run()
+
+
+
+    # Updated Param Sheet
+    upload_params['Last Run Date'] = upload_params['Last Run Date'].astype(str)
+    upload_params = upload_params.reset_index()
+    uploadparms_sheet.update([upload_params.columns.values.tolist()] + upload_params.values.tolist())
+
---
+++ utils.py
@@ -0,0 +1,72 @@
+import os
+import zipfile
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas_gbq
+import numpy as np
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import pandas as pd
+import zipfile
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+
+
+
+
+def unzip_csv(file):
+    zf = zipfile.ZipFile(file)
+    csv_file_name = zf.namelist()[0]
+    final_csv_data = zf.open(csv_file_name)
+    return final_csv_data
+
+dir_path = os.getcwd()
+
+def saveList(myList,filename):
+    # the filename should mention the extension 'npy'
+    np.save(filename,myList)
+    print("Saved successfully!")
+
+
+
+import smtplib
+def send_email(from_email, from_email_pass, to_email, subject, body_text):
+    #Ports 465 and 587 are intended for email client to email server communication - sending email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    #starttls() is a way to take an existing insecure connection and upgrade it to a secure connection using SSL/TLS.
+    server.starttls()
+    #Next, log in to the server
+    server.login(from_email, from_email_pass)#("prreporting@pivotroots.com", "vwcuvekcfubywxcg")
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    body = MIMEText(body_text)
+    msg["From"] = from_email#"prreporting@pivotroots.com"
+    msg["To"] = to_email#"ekagra.singh@pivotroots.com"
+    msg.attach(body)
+    server.sendmail(msg["From"], msg["To"],msg.as_string())
+    server.quit()
+
+#Send the mail
+#tanmay.sankhe@pivotroots.com
+
+def get_uploadparams():
+    #Authorizing the API
+    scope = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://spreadsheets.google.com/feeds'
+        ]
+    key_file = 'dashbaording-all-clients-e80f9ab53385.json'
+    creds = ServiceAccountCredentials.from_json_keyfile_name(key_file,scope)
+    client = gspread.authorize(creds)
+    worksheet = client.open('uploadparams')
+    uploadparms_sheet = worksheet.get_worksheet(0)
+    upload_params_dict = uploadparms_sheet.get_all_records()
+    upload_params = pd.DataFrame.from_dict(upload_params_dict)
+    upload_params.set_index('ID', inplace = True)
+    upload_params['Last Run Date'] = pd.to_datetime(upload_params['Last Run Date'], format='%Y-%m-%d')
+    return uploadparms_sheet, upload_params
+
+
+####
